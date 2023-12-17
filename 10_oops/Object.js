53db947c195775294ea643b5d2749ec02397602e
Sujeet Kumar <88089946+sujeet-kr1415@users.noreply.github.com>
2023-12-17 19:10:01 +0000

    new keyword

---
+++ 10_oops/Object.js
@@ -0,0 +1,44 @@
+// js is prototyped inherital
+// everything in js is object means to say that a function can be a function but at the same time its is an object
+
+function multipleBy5(num){
+    return num*5
+}
+
+multipleBy5.power = 2
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);  // 2
+console.log(multipleBy5.prototype);  // {}
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`${this.username} scores ${this.score}`);
+}
+// now the most important thing is the "new keyword"
+const player1 = new createUser("Sujeet", 23)
+const player2 = new createUser("Abhisek", 32)
+
+player1.printMe()
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
